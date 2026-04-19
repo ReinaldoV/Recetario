8aa42f993bf7b4597186ea31b636bf1dbb065005
Reinaldo <reinaldovillajavi@gmai.com>
2026-04-19 10:18:40 +0200

    Initial Commit

---
+++ Recetario/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Recetario
+//
+//  Created by Reinaldo Villanueva on 19/4/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
